7de5b9c545edb774e5b170421b552ca33c31dec1
James Edmonds <james@deckchairlabs.com>
2022-05-14 14:43:53 +0000

    fixed!

--- server.tsx
+++ server.tsx
@@ -1,5 +1,5 @@
-import React from "https://esm.sh/react@18?dev";
-import { renderToStream } from "https://esm.sh/react-streaming/server?dev";
+import React, { Suspense } from "https://esm.sh/react@18?dev";
+import { renderToStream } from "https://esm.sh/react-streaming@0.2.8/server?dev";
 import { serve } from "https://deno.land/std@0.139.0/http/server.ts";
 
 const port = 8080;
@@ -9,7 +9,24 @@ function App() {
 }
 
 const handler = async (): Promise<Response> => {
-  const { readable } = await renderToStream(<App />);
+  const { readable, injectToStream } = await renderToStream(
+    <html lang="en">
+      <head>
+        <title>Stream</title>
+      </head>
+      <body>
+        <Suspense>
+          <App />
+        </Suspense>
+      </body>
+    </html>,
+    {
+      disable: true,
+      webStream: true,
+    },
+  );
+
+  injectToStream("<div>Injected</div>");
 
   return new Response(readable, {
     status: 200,
